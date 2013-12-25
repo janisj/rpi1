65c353987590bae76309db645e0f46b347196776
bee <bee@bee.(none)>
2013-12-25 21:24:24 +0200

    remove references to apc

--- wwwrpi/lib.php
+++ wwwrpi/lib.php
@@ -4,19 +4,6 @@ function get_config()
 		
 		$file = file_get_contents('./configure.json',false);
 		return $file;
-/* //needs apc installation on php belov 5.5... 
 
-	if (apc_exists('config')) {
-		return  apc_fetch('config');
-	} else {
-		
-		$file = file_get_contents('./configure.json',false);
-		if ($file==FALSE){
-		return FALSE;
-		}
-    		apc_store('config',$file,60*60);
-		return $file;
-	}	
-*/
 }
 ?>
